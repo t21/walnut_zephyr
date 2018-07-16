f0487611e490190766420c50688ae30da5f8c714
Thomas Berg <thomas.berg2@gmail.com>
2018-07-16 22:13:01 +0200

    Delay first ambient light measurement to 5s

--- src/als.c
+++ src/als.c
@@ -70,6 +70,6 @@ void als_init(void)
 
     k_work_init(&meas_work, meas_work_handler);
     k_timer_init(&meas_timer, meas_timer_handler, NULL);
-    k_timer_start(&meas_timer, K_SECONDS(1), K_SECONDS(60));
+    k_timer_start(&meas_timer, K_SECONDS(5), K_SECONDS(60));
 #endif
 }

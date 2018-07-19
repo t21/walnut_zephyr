b083dddc6955bea85c313273c1815c81462bf3ec
Thomas Berg <thomas.berg2@gmail.com>
2018-07-19 14:46:11 +0200

    Clean up BMP280 sensor

--- drivers/bmp280/bmp280.h
+++ drivers/bmp280/bmp280.h
@@ -18,17 +18,11 @@
 
 #define BMP280_REG_PRESS_MSB            0xF7
 #define BMP280_REG_COMP_START           0x88
-// #define BME280_REG_HUM_COMP_PART1       0xA1
-// #define BME280_REG_HUM_COMP_PART2       0xE1
 #define BMP280_REG_ID                   0xD0
 #define BMP280_REG_CONFIG               0xF5
 #define BMP280_REG_CTRL_MEAS            0xF4
-// #define BME280_REG_CTRL_HUM             0xF2
 
-// #define BMP280_CHIP_ID_SAMPLE_1         0x56
-// #define BMP280_CHIP_ID_SAMPLE_2         0x57
 #define BMP280_CHIP_ID               	0x58
-// #define BME280_CHIP_ID                  0x60
 #define BMP280_MODE_NORMAL              0x03
 #define BMP280_SPI_3W_DISABLE           0x00
 
@@ -55,18 +49,6 @@
 #elif defined CONFIG_BMP280_PRESS_OVER_16X
 #define BMP280_PRESS_OVER               (5 << 2)
 #endif
-
-// #if defined CONFIG_BME280_HUMIDITY_OVER_1X
-// #define BME280_HUMIDITY_OVER            1
-// #elif defined CONFIG_BME280_HUMIDITY_OVER_2X
-// #define BME280_HUMIDITY_OVER            2
-// #elif defined CONFIG_BME280_HUMIDITY_OVER_4X
-// #define BME280_HUMIDITY_OVER            3
-// #elif defined CONFIG_BME280_HUMIDITY_OVER_8X
-// #define BME280_HUMIDITY_OVER            4
-// #elif defined CONFIG_BME280_HUMIDITY_OVER_16X
-// #define BME280_HUMIDITY_OVER            5
-// #endif
 
 #if defined CONFIG_BMP280_STANDBY_05MS
 #define BMP280_STANDBY                  0
@@ -99,54 +81,46 @@
 #endif
 
 #define BMP280_CTRL_MEAS_VAL            (BMP280_PRESS_OVER | \
-					 BMP280_TEMP_OVER |  \
-					 BMP280_MODE_NORMAL)
+                     BMP280_TEMP_OVER |  \
+                     BMP280_MODE_NORMAL)
 #define BMP280_CONFIG_VAL               (BMP280_STANDBY | \
-					 BMP280_FILTER |  \
-					 BMP280_SPI_3W_DISABLE)
+                     BMP280_FILTER |  \
+                     BMP280_SPI_3W_DISABLE)
 
 #define BMP280_I2C_ADDR                 CONFIG_BMP280_I2C_ADDR
 
 struct bmp280_data {
 #ifdef CONFIG_BMP280_DEV_TYPE_I2C
-	// struct device *i2c_master;
-	struct device *i2c_wrap;
-	u16_t i2c_slave_addr;
+    struct device *i2c_wrap;
+    u16_t i2c_slave_addr;
 #elif defined CONFIG_BMP280_DEV_TYPE_SPI
-	struct device *spi;
-	struct spi_config spi_cfg;
+    struct device *spi;
+    struct spi_config spi_cfg;
 #else
 #error "BMP280 device type not specified"
 #endif
-	/* Compensation parameters. */
-	u16_t dig_t1;
-	s16_t dig_t2;
-	s16_t dig_t3;
-	u16_t dig_p1;
-	s16_t dig_p2;
-	s16_t dig_p3;
-	s16_t dig_p4;
-	s16_t dig_p5;
-	s16_t dig_p6;
-	s16_t dig_p7;
-	s16_t dig_p8;
-	s16_t dig_p9;
-	// u8_t dig_h1;
-	// s16_t dig_h2;
-	// u8_t dig_h3;
-	// s16_t dig_h4;
-	// s16_t dig_h5;
-	// s8_t dig_h6;
+    /* Compensation parameters. */
+    u16_t dig_t1;
+    s16_t dig_t2;
+    s16_t dig_t3;
+    u16_t dig_p1;
+    s16_t dig_p2;
+    s16_t dig_p3;
+    s16_t dig_p4;
+    s16_t dig_p5;
+    s16_t dig_p6;
+    s16_t dig_p7;
+    s16_t dig_p8;
+    s16_t dig_p9;
 
-	/* Compensated values. */
-	s32_t comp_temp;
-	u32_t comp_press;
-	// u32_t comp_humidity;
+    /* Compensated values. */
+    s32_t comp_temp;
+    u32_t comp_press;
 
-	/* Carryover between temperature and pressure/humidity compensation. */
-	s32_t t_fine;
+    /* Carryover between temperature and pressure/humidity compensation. */
+    s32_t t_fine;
 
-	u8_t chip_id;
+    u8_t chip_id;
 };
 
 #define SYS_LOG_DOMAIN "BMP280"
